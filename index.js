914725b2e06fa95bea3b0872ffb4cfbc1e4f675c
juliana <nani.jimenez@hotmail.com>
2020-10-03 12:36:13 -0500

    fs

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-module.exports = () => {
-  // ...
-};
+/*module.exports = () => {
+  
+};*/
+/*const os = require ('os')
+
+let cpu = os.cpus();
+let sistema = os.platform();
+let usuario = os.hostname(); 
+
+console.log(usuario);
+//console.log(cpu);
+console.log(sistema);*/
+
+
+
+console.log('holi')
+
